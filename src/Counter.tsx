b40bee340245ef6e61b6f5d2eb90d3ad4de918de
takaya <takaya39899805@gmail.com>
2024-02-08 04:10:22 +0900

    feat: counter

---
+++ src/Counter.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from './store';
+import { decremented, incremented, incrementedByAmount } from './counterSlice';
+
+const Counter: React.FC = () => {
+  // Reduxストアからカウンターの値を取得
+  const counterValue = useSelector((st: RootState) => st.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Counter</h2>
+      <div>
+        <button onClick={() => dispatch(decremented())}>-</button>
+        <span>{counterValue}</span>
+        <button onClick={() => dispatch(incremented())}>+</button>
+      </div>
+      <div>
+        <button onClick={() => dispatch(incrementedByAmount(5))}>+5</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
