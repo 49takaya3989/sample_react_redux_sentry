5677db1116f59d5a43bc8f2a61796b5af954564b
takaya <takaya39899805@gmail.com>
2024-02-08 15:26:09 +0900

    feat: func sending redux action type to sentry

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,33 @@
 // src/store/store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counterSlice';
+import { createReduxEnhancer } from '@sentry/react';
 
+const sentryReduxEnhancer = createReduxEnhancer({
+  actionTransformer: (action) => {
+    if (action.type === "GOVERNMENT_SECRETS") {
+      // Return null to not log the action to Sentry
+      return null;
+    }
+    if (action.type === "SET_PASSWORD") {
+      // Return a transformed action to remove sensitive information
+      return {
+        ...action,
+        password: null,
+      };
+    }
+
+    return action;
+  },
+});
 
 export const store = configureStore({
   reducer: {
     // ここに各Reducerを追加します
     counter: counterReducer,
+  },
+  enhancers: (getDefaultEnhancers) => {
+    return getDefaultEnhancers().concat(sentryReduxEnhancer);
   },
 });
 
